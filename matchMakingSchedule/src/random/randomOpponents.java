5f6d6d8f675faa450c6fc2867824439b26c67348
S331474817 <S331474817@LDHS-BCT-100775.ad.ocdsb.ca>
2020-01-22 09:23:15 -0500

    add comments

--- matchMakingSchedule/src/random/randomOpponents.java
+++ matchMakingSchedule/src/random/randomOpponents.java
@@ -18,11 +18,12 @@ public class randomOpponents {
     int teamNum;
     teams[]teams;    
     int numTeam=8;
-        boolean[] hasTeam;
-        boolean valid=false;
-        int opp;
-        int opp2;
-    
+    boolean[] hasTeam;
+    boolean valid=false;
+    int opp;
+    int opp2;
+        
+    //get the different team information
     public void getTeams(teams[] teamInfo){
         teamNum=teamInfo.length;
         teams =new teams[teamInfo.length];
@@ -37,25 +38,23 @@ public class randomOpponents {
         }
         
     }
+    //get the matchInfo
     public void getMatches(String[][][][] matchInfo){
         matches=matchInfo;
     }
+    //initiate values
     public void initVal(int pNum){
         hasTeam=new boolean[teamNum];
         parkNum=pNum;
     }
+    //randomizes the opponents
     public void normRand(int matchNum){
         Random rand=new Random();
-        //System.out.println("hello");
         for(int x=0;x<hasTeam.length;x++){
             hasTeam[x]=false;
         }
         for(int x=0;x<2;x++){
-           // System.out.println("setting teams for time "+x);
-           // System.out.println("parkNum" +parkNum);
             for(int y=0;y<parkNum;y++){
-           //     System.out.println("Helo");
-           //     System.out.println("setting teams for park "+y);
                 do{
                     System.out.println("selecting first team");
                     opp=rand.nextInt(teamNum);
@@ -63,15 +62,12 @@ public class randomOpponents {
                     System.out.println(teams[opp].name);
                     if(hasTeam[opp]==true){
                         valid=false;
-            //             System.out.println("team already chosed");
                     }                     
                     else{
                         valid=true;
                         hasTeam[opp]=true;
-            //            System.out.println("team chosen "+opp);
                     }                    
                 }while(valid==false);
-            //    System.out.println("assinging 1 team to match making");
                 MatchMakingSchedule.matchInfo[matchNum][x][y][0]=teams[opp].name;
                 do{
                     System.out.println("selecting second team");
@@ -80,49 +76,33 @@ public class randomOpponents {
                     System.out.println(teams[opp2].name);
                     if(hasTeam[opp2]==true){
                         valid=false;
-            //            System.out.println("team already chosed");
                     }                    
                     else{
                         valid=true;
                         hasTeam[opp2]=true;
-            //            System.out.println("team chosen "+opp2);
                     }            
-            //        System.out.println("assinging teams to already played");
                     for(int z=0;z<teamNum-1;z++){
-             //           System.out.println(alreadyPlayed[opp2][z]);
                         if(alreadyPlayed[opp2][z]==true){
                             valid=false;
                         }
                         else if(valid==true){
                             valid=true;
-                            //alreadyPlayed[opp][z]=true;
-            /*                System.out.println();
-                            System.out.println("opp2: "+opp2);
-                            System.out.println("opp: "+opp);
-            */                if(opp2<=opp){
-            //                    System.out.println("opp2<opp");
+                            if(opp2<=opp){            
                                 teams[opp].prevOpp[opp2]=true;
                             }
                             else if(opp2>opp){
-            //                    System.out.println("opp2>opp");
                                 teams[opp].prevOpp[opp2-1]=true;
-                            }
-                            
+                            }                            
                             if(opp<=opp2){
-            //                    System.out.println("opp<opp2");
                                 teams[opp2].prevOpp[opp]=true;
                             }
                             else if(opp>opp2){
-            //                    System.out.println("opp>opp2");
                                 teams[opp2].prevOpp[opp-1]=true;
                             }
                             break;
-                            
                         }
                     }
                 }while(valid==false);
-                    
-                System.out.println("assinging 2 team to matches");
                 MatchMakingSchedule.matchInfo[matchNum][x][y][1]=teams[opp2].name;                
             }
         }
