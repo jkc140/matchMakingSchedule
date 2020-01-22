4c5d49b5cb742b1945e3e9f11f5f6aba2782dc28
S331474817 <S331474817@LDHS-BCT-100775.ad.ocdsb.ca>
2020-01-22 09:23:44 -0500

    add comments

--- matchMakingSchedule/src/matchmakingschedule/fileReader.java
+++ matchMakingSchedule/src/matchmakingschedule/fileReader.java
@@ -21,19 +21,19 @@ public class fileReader {
         String[] startDate=new String[2];
         String[] endDate=new String[2];
         String[] matchNum=new String[2];
+    //get file name
     public void getName(String name){	
         fName=name;	
         fName=name+".txt"; // got to make it so it checks to see if .txt is on it	
     }	
+    //get the information out of file
     public void fileRead(){	
         System.out.println(fName);
         File dataFile=new File(fName);	
         try{	
             if(dataFile.createNewFile()==true){ 	
-                System.out.println("Creating"); //delete me	
             }	
             else{                	
-                System.out.println("File is already there"); //delete me	
             }	
 
             Scanner arrayLength=new Scanner(dataFile);	
@@ -49,7 +49,6 @@ public class fileReader {
             int count=0;	
             while(storeData.hasNextLine()==true){	
                 rawData[count]=storeData.nextLine();	
-    //            System.out.println(rawData[count]);
                 count++;
                 
             }	
@@ -62,13 +61,14 @@ public class fileReader {
         finally{	
         }	
     }	
+    //store the information into different arrays that we can use
     public void sort(){	
+        System.out.println(rawData.length);
         for(int x=0;x<rawData.length;x++){
             System.out.println(rawData[x]);
         }	
         String[] teamNum=new String[2];	
         teamNum=rawData[0].split("/");	
-    //    System.out.println("Team Num: "+teamNum[1]);	
         numTeam=Integer.valueOf(teamNum[1]);	
         int max=0;	
         int count=0;	
@@ -79,37 +79,22 @@ public class fileReader {
             while(tempHolder.indexOf("/")!=-1){	
                 tempHolder=tempHolder.replaceFirst("/", "0");	
                 count++;	
-     //           System.out.println("Looping");	
             }	
             splitter[x-3]=count;	
-   //         System.out.println(count);	
             if (count>max){	
                 max=count;	
             }	
             count=0;	
         }	
-    //    System.out.println("text splittiung");	
-        for(int x=0;x<numTeam;x++){	
-    //        System.out.println(splitter[x]);	
-        }	
-   //     System.out.println("text splittiung");	
-   //     System.out.println(max);	
+	
         teamInfo=new String[numTeam][max+2];	
         for(int x=0;x<numTeam;x++){	
             teamInfo[x][max+1]=String.valueOf(splitter[x]);	
         }	
         for(int x=3;x<3+numTeam;x++){	
             teamInfo[x-3]=rawData[x].split("/");	
-            //System.out.println(lineStore[x]);	
         }	
-        for(int x=0;x<numTeam;x++){	
-           // System.out.println("changing line");	
-    //        System.out.println(splitter[x]+1);	
-            for(int y=0;y<splitter[x]+1;y++){	
-               // System.out.println("Spot");	
-    //            System.out.println(teamInfo[x][y]);	
-            }	
-        }	
+	
         int skipCount=3+numTeam+2;
         String tempHolder=rawData[skipCount];
             while(tempHolder.indexOf("/")!=-1){
@@ -117,29 +102,12 @@ public class fileReader {
                 count++;;
             }
         parkInfo=new String[count];
-    //    System.out.println("parks: ");
-        //for(int x=skipCount;x<3+numTeam;x++){
             parkInfo=rawData[skipCount].split("/");
-            //System.out.println(lineStore[x]);
-            for(int x=0;x<parkInfo.length;x++){
-   //             System.out.println(parkInfo[x]);
-
-            }
-            skipCount=skipCount+2;
-    //        System.out.println(skipCount);
-                        /*System.out.println(skipCount+1);
-                        System.out.println(skipCount+2);
-                        System.out.println(skipCount+3);   */        
+            skipCount=skipCount+2;     
             startDate=rawData[skipCount].split("/");
             endDate=rawData[skipCount+1].split("/");
             matchNum=rawData[skipCount+2].split("/");
-           /* for(int x=0;x<startDate.length;x++){
-                System.out.println("startDate");
-                 System.out.println(startDate[x]);
-            }*/
-    //        System.out.println(startDate[1]);
-   //         System.out.println(endDate[1]);
-   //         System.out.println(matchNum[1]);
+
     }	
   
     int matchNum2=0;
@@ -149,109 +117,54 @@ public class fileReader {
     public String[][][][] matchInfo;
     public String[][]parkDetail;
     public String[]parkList;    
+    //read the file we store our data in
     public void storeRead(){
         System.out.println("store read");
         File storeFile=new File("Schedule.txt");
         try{
             Scanner length=new Scanner(storeFile);
             while(length.hasNextLine()==true){	
-                System.out.println("hello");
                 matchNum2++;	
                 length.nextLine();	
             }
             length.close();
-            System.out.println("matchNum: "+matchNum2);
             rawData2=new String[matchNum2];
             System.out.println(rawData2);
             Scanner storing=new Scanner(storeFile);
             int count=0;	
             while(storing.hasNextLine()==true){	
                 rawData2[count]=storing.nextLine();	
-                System.out.println("line: "+rawData2[count]);
                 count++;	
             }
-            
-            
-            
-            
         }
         catch(Exception e){
-            
         }
     }
+    //make the data usable
     public void storeSort(){
-        System.out.println("store stort");
         String tempHolder;
-        //for(int x=0;x<rawData2.length;x++){
             tempHolder=rawData2[0];
             while(tempHolder.indexOf("/")!=-1){
-                System.out.println("parkNum1: "+parkNum);
                 parkNum++;
-                tempHolder=tempHolder.replaceFirst("/", "0");	
-                
-                System.out.println("parkNum2: "+parkNum);
-            }
-        //}
-        System.out.println("parkNum: "+parkNum);
+                tempHolder=tempHolder.replaceFirst("/", "0");        
+            }        
         matchInfo=new String[matchNum2][parkNum][2][2];
         parkArray=new String[matchNum2][parkNum];
-        /*System.out.println("here 1");
-        for(int x=0;x<matchNum2;x++){
-            System.out.println("here 1.1");
-            
-            for(int y=0;y<parkNum;y++){
-                System.out.println("here 2");
-                parkArray[x]=rawData2[x].split("/");
-                System.out.println("parkArray[y]: "+parkArray[0][0]);
-                System.out.println("parkArray[y]: "+parkArray[1]);
-            }
-            
-        }*/
-        System.out.println("here 2");
         parkDetail=new String[parkNum][7];
         for(int x=0;x<matchNum2;x++){     
-            System.out.println("parkNum 2: "+parkNum);
-            for(int y=0;y<parkNum;y++){
-               /* System.out.println("x: "+x);
-                System.out.println("y: "+y);
-               
-                System.out.println("parkDetail: "+parkDetail.length);
-                System.out.println("parkArray[x][y]: "+parkArray[x].length);
-                System.out.println("parkNum: "+parkNum);*/
-               
+            for(int y=0;y<parkNum;y++){               
                 parkArray[x]=rawData2[x].split("/");
-                System.out.println("parkArray[y]: "+parkArray[x][y]);
                 parkDetail[y]=parkArray[x][y].split("&");
-                //System.out.println(parkDetail[y].length);
-                for(int a=0;a<parkDetail[y].length;a++){
-                    
-
-                    
-                        System.out.println(a);
-                        System.out.println(parkDetail[y][a]);
-
-                }
-                    matchInfo[x][y][0][0]=parkDetail[y][2];
-                    matchInfo[x][y][0][1]=parkDetail[y][3];
-                    matchInfo[x][y][1][0]=parkDetail[y][5];
-                    matchInfo[x][y][1][1]=parkDetail[y][6];
-                
-                
-                
-                
+                matchInfo[x][y][0][0]=parkDetail[y][2];
+                matchInfo[x][y][0][1]=parkDetail[y][3];
+                matchInfo[x][y][1][0]=parkDetail[y][5];
+                matchInfo[x][y][1][1]=parkDetail[y][6];
             }
-            System.out.println("next match");
         }
        
-        System.out.println("here 3");
         parkList=new String[parkNum];
         for(int x=0;x<parkNum;x++){
-            System.out.println(parkDetail[0]);
-            parkList[x]=parkDetail[x][0];
-            
+            parkList[x]=parkDetail[x][0];            
         }
-        System.out.println("end");
-
-    }
-    
+    }    
 }
