33ffe8c054bae6607d019c36572ace84898a3412
J C <Joshua@192.168.1.22>
2020-01-21 23:11:03 -0500

    adding create button function

--- matchMakingSchedule/src/MainMenu.java
+++ matchMakingSchedule/src/MainMenu.java
@@ -118,8 +118,8 @@ String [][][][] matches;
     }//GEN-LAST:event_jButton1ActionPerformed
 
     private void jButton2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton2ActionPerformed
-        // TODO add your handling code here:
-        matches = MatchMakingSchedule.tCaseMatches;
+        new GetFileName().setVisible(true);
+        this.setVisible(false);
         //createSchedule();
     }//GEN-LAST:event_jButton2ActionPerformed
 
